ea3bb0b737882dda6c5a9e5bac7f5f9736dee5ac
ahetland <a.hetland@web.de>
2018-02-23 14:42:18 +0100

    Required check for vendor invoice number

--- ah_extensions/models/account_ah_ext.py
+++ ah_extensions/models/account_ah_ext.py
@@ -30,6 +30,28 @@ class AccountBankStatement(models.Model):
 
 
 
+class AccountInvoice(models.Model):
+    """To force the entry of an invoice number when invoice is received."""
+
+    _inherit = "account.invoice"
+    _description = "Invoice"
+
+
+    def _check_invoice_reference(self):
+        for invoice in self:
+            #refuse to validate a vendor bill/refund if there already exists one with the same reference for the same partner,
+            #because it's probably a double encoding of the same bill/refund
+            
+            #if invoice.type in ('in_invoice', 'in_refund') and invoice.reference:
+            #when removing the and inv.reference we make ref mandatory!
+
+            if invoice.type in ('in_invoice', 'in_refund'):
+                if not invoice.reference:
+                    raise UserError(_("Vendor reference or invoice number is required."))
+
+                if self.search([('type', '=', invoice.type), ('reference', '=', invoice.reference), ('company_id', '=', invoice.company_id.id), ('commercial_partner_id', '=', invoice.commercial_partner_id.id), ('id', '!=', invoice.id)]):
+                    raise UserError(_("Duplicated vendor reference detected. You probably encoded twice the same vendor bill/refund."))
+
 
 
 class AccountJournal(models.Model):
@@ -68,3 +90,8 @@ class AccountPayment(models.Model):
             if not self.journal_id.bank_account_id.bank_bic:
                 raise ValidationError(_("The account '%s' (journal %s) requires a Bank Identification Code (BIC) to pay via SEPA. Please configure it first.")
                     % (self.journal_id.bank_account_id.acc_number, self.journal_id.name))
+
+
+
+
+                    
